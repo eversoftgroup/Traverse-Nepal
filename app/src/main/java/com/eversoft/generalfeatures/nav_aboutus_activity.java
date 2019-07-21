98db4cd3d8991bfcc98ff928ab87470cd89407f9
Pradeep Poudel <systemanalyst2054@gmail.com>
2019-07-21 23:27:03 +0545

    home navigation completed

---
+++ app/src/main/java/com/eversoft/generalfeatures/nav_aboutus_activity.java
@@ -0,0 +1,15 @@
+package com.eversoft.generalfeatures;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.eversoft.traversenepal.R;
+
+public class nav_aboutus_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.nav_aboutus_activity);
+    }
+}
---
+++ app/src/main/java/com/eversoft/settings/nav_setting_activity.java
@@ -0,0 +1,15 @@
+package com.eversoft.settings;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.eversoft.traversenepal.R;
+
+public class nav_setting_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.nav_settings_activity);
+    }
+}
