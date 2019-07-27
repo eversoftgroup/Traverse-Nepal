0c86f007a99ba5a3c38a85905db71fc0ec4773f6
PrabhuGurung <40710025+PrabhuGurung@users.noreply.github.com>
2019-07-27 22:37:04 +0545

    menu inflater still to be done

--- app/src/main/java/com/eversoft/traversenepal/nav_mypick_activity.java
+++ app/src/main/java/com/eversoft/traversenepal/nav_mypick_activity.java
@@ -2,14 +2,27 @@ package com.eversoft.traversenepal;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.PopupMenu;
 
 import com.eversoft.traversenepal.R;
 
-public class nav_mypick_activity extends AppCompatActivity {
+public class nav_mypick_activity extends AppCompatActivity implements PopupMenu {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.nav_mypick_activity);
+
+
+
+    }
+
+    public void showPopup(View v){
+        PopupMenu popup = new PopupMenu(this, v);
+        popup.setOnMenuItemClickListener((PopupMenu.OnMenuItemClickListener) this);
     }
 }
