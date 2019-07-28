9ab68e18591e01960665782caa19034370b6ecef
dhpradeep <dhpradeep25@gmail.com>
2019-07-29 00:03:37 +0545

    Added my-pick feature from /Pradeep branch

--- app/src/main/java/com/eversoft/traversenepal/nav_mypick_activity.java
+++ app/src/main/java/com/eversoft/traversenepal/nav_mypick_activity.java
@@ -2,6 +2,9 @@ package com.eversoft.traversenepal;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.PopupMenu;
 
 import com.eversoft.traversenepal.R;
 
@@ -11,5 +14,18 @@ public class nav_mypick_activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.nav_mypick_activity);
+        setTitle("My Pick");
+
+        final ImageButton BtnMoreOption = findViewById(R.id.nav_my_pick_more_vert);
+
+        BtnMoreOption.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                PopupMenu popupMenu = new PopupMenu(nav_mypick_activity.this, BtnMoreOption);
+                //inflating xml file
+                popupMenu.getMenuInflater().inflate(R.menu.my_pic_card_menu, popupMenu.getMenu());
+                popupMenu.show();
+            }
+        });
     }
 }
